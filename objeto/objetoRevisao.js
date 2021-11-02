7c0672fc1ae44fa2d63c3d0064b04780b738f69e
netryxdev <adrianoneto2501@gmail.com>
2021-11-02 01:23:07 -0300

    commit aleatorio

---
+++ objeto/objetoRevisao.js
@@ -0,0 +1,3 @@
+// Coleção dinâmica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+// OO = Orientação a Objeto
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+         // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Princípios importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança
+// 4. Polimorfismo
